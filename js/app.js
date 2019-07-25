114d7ca98dc4abc3e014ea395cd81e00b2dd59b8
Kyle Scheuerlein <kyle@kylesureline.com>
2019-07-24 22:31:01 -0700

    implement smooth scrolling

---
+++ js/app.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+
+  const body = document.querySelector('body');
+  const cardColorGrid = document.querySelector('.card__color-grid');
+  const navLinks = document.querySelector('.sidebar');
+
+  cardColorGrid.addEventListener('click', (e) => {
+      const target = e.target;
+      const className = target.className;
+      if(className.includes('shades--') || className.includes('brand-colors--')) {
+        const p = target.querySelector('.color-caption');
+        const textArea = document.createElement('textarea');
+        body.appendChild(textArea);
+        textArea.value = p.textContent;
+        textArea.select();
+        document.execCommand('copy');
+        console.log(textArea.value);
+        body.removeChild(textArea);
+
+        const tooltip = document.createElement('span');
+        tooltip.className = 'tooltip';
+        tooltip.textContent = 'Copied to clipboard!';
+        target.appendChild(tooltip);
+        setTimeout(() => {
+          tooltip.style.opacity = 0;
+          setTimeout(() => {
+            target.removeChild(tooltip);
+            }, 2000);
+          }, 500);
+      }
+    });
+
+    navLinks.addEventListener('click', (e) => {
+      if(e.target.tagName === 'A') {
+        let target = e.target;
+        let scrollTo = '#' + target.href.split('#')[1];
+        document.querySelector(scrollTo).scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+
+});
